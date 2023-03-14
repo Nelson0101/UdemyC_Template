93e55d0be758b4c848aaf35f86b3aacbf57fe08a
gaempnil <gaempnil@students.zhaw.ch>
2023-03-14 19:00:58 +0100

    First Commit

--- Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
+++ Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int age;
+    printf("Please enter your Age");
+    scanf("%d", &age);
+    printf("Dein Alter ist: %d \n", age);
+    printf("Youre living already %d days \n", age*365);
+    printf("You are living already %d hours\n", age*365*24);
+    printf("you are living already %d minutes\n", age*365*24*60);
+    printf("you are living already %d seconds\n", age*365*24*60*60);
+
+
+
 
 
     return 0;
--- Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
+++ Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
+
 
 int main()
 {
+    int16_t hello = 12;
+    printf("%d", hello);
 
     return 0;
 }
